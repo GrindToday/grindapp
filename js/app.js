7973ed961c5429d42a4eafc1170b3cdb805818f1
Oskar Jarczyk <oskar.jarczyk@gmail.com>
2017-02-11 15:39:35 +0100

    New footer
    Why:
    
    *
    
    This change addresses the need by:
    
    * need to allow to close this remainder in the future

--- js/app.js
+++ js/app.js
@@ -16,4 +16,23 @@ $(function () {
     var randomized = Math.round(19000 + (Math.random() * 899));
 
     $('body').css('backgroundImage', 'url(https://kbdevstorage1.blob.core.windows.net/asset-blobs/' + randomized + '_en_1)');
+
+    CoinWidgetCom.go({
+        wallet_address: '1AYLrvxxtUANbfPH9vxmtY5xvNZoMbgoyX',
+        currency: 'bitcoin',
+        counter: 'hide',
+        lbl_button: 'Support me with 1 click',
+        lbl_count: 'donations',
+        lbl_amount: 'BTC',
+        lbl_address: 'Use address below to donate. Thanks!',
+        qrcode: true,
+        alignment: 'bl',
+        decimals: 8,
+        size: "small",
+        color: "light",
+        countdownFrom: "0.04",
+        element: "#coinwidget-bitcoin-1AYLrvxxtUANbfPH9vxmtY5xvNZoMbgoyX",
+        onShow: function(){},
+        onHide: function(){}
+    });
 });
