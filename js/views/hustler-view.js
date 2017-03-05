3e77b4d7b7376ab7622e9c5cda84dc4348d0ca8d
Oskar Jarczyk <oskar.jarczyk@gmail.com>
2017-03-05 16:59:19 +0100

    Clears the "Hustler" temporary text
    Why:
    
    * clearing information on clicking the name
    
    This change addresses the need by:
    
    * coding js
    
    # 50-character subject line
    #
    # 72-character wrapped longer description.

--- js/views/hustler-view.js
+++ js/views/hustler-view.js
@@ -1,3 +1,20 @@
 /**
  * Created by Oskar on 22.02.2017.
  */
+
+nameinput_onceclicked = false;
+
+$('#nameinput').click(function() {
+    if(!nameinput_onceclicked) {
+        $('#nameinput').css({
+            'text-decoration': 'none'
+        });
+        // $('#nameinput').text($('#nameinput').text().replace('*', ''));
+        $('#nameinput').text('');
+        $('#nameinput').focus();
+
+        $('#clicktoedit').html('</br>');
+
+        nameinput_onceclicked = true;
+    };
+});
